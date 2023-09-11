fd36ef747fe07cdbf1eb6bc81743949d59f3411f
César González <chesarglez429@gmail.com>
2023-09-11 15:21:14 -0600

    :construction: Create parser to match a word and some examples

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+enum Result {
+  case Ok;
+  case Err;
+};
+
+
+$parse_word = fn($word_to_match) => function($input) use ($word_to_match) {
+  $type_of_input = gettype($input);
+  if ($type_of_input !== 'string') {
+    return [Result::Err, "Expecting a string. Got a '$type_of_input' instead"];
+  }
+
+  $word_to_match_length = strlen($word_to_match);
+  $input_start = substr($input, 0,  $word_to_match_length);
+  if ($input_start !== $word_to_match) {
+    return [Result::Err, "Expecting '$word_to_match'. Got '$input_start' instead"];
+  }
+  
+  $input_rest = substr($input, $word_to_match_length);
+  return [Result::Ok, $input_start, $input_rest];
+};
+
+$pwebos = $pword('webos');
+print_r($pwebos('webos revueltos'));
+print_r($pwebos('webos motuleños'));
+print_r($pwebos('longaniza'));
+print_r($pwebos('web'));
+?>
