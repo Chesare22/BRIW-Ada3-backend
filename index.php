a8ece8ff35fa53594c4211803f2ffa5a58b47ec7
César González <chesarglez429@gmail.com>
2023-09-14 10:07:54 -0600

    :recycle: Rename parsers

--- index.php
+++ index.php
@@ -5,24 +5,24 @@ enum Result {
 };
 
 
-$parse_word = fn($word_to_match) => function($input) use ($word_to_match) {
+$p_string = fn($string_to_match) => function($input) use ($string_to_match) {
   $type_of_input = gettype($input);
   if ($type_of_input !== 'string') {
     return [Result::Err, "Expecting a string. Got a '$type_of_input' instead", $input];
   }
 
-  $word_to_match_length = strlen($word_to_match);
-  $input_start = substr($input, 0,  $word_to_match_length);
-  if ($input_start !== $word_to_match) {
-    return [Result::Err, "Expecting '$word_to_match'. Got '$input_start' instead", $input];
+  $string_to_match_length = strlen($string_to_match);
+  $input_start = substr($input, 0,  $string_to_match_length);
+  if ($input_start !== $string_to_match) {
+    return [Result::Err, "Expecting '$string_to_match'. Got '$input_start' instead", $input];
   }
   
-  $input_rest = substr($input, $word_to_match_length);
+  $input_rest = substr($input, $string_to_match_length);
   return [Result::Ok, $input_start, $input_rest];
 };
 
 
-$parse_and_then = fn($parser1, $parser2) => function($input) use ($parser1, $parser2) {
+$p_and_then = fn($parser1, $parser2) => function($input) use ($parser1, $parser2) {
   $output1 = $parser1($input);
   [$result1, $value1, $remaining1] = $output1;
   if($result1 === Result::Err) {
@@ -40,7 +40,7 @@ $parse_and_then = fn($parser1, $parser2) => function($input) use ($parser1, $par
 };
 
 
-$parse_or_else = fn($parser1, $parser2) => function($input) use ($parser1, $parser2) {
+$p_or_else = fn($parser1, $parser2) => function($input) use ($parser1, $parser2) {
   $output1 = $parser1($input);
   [$result1] = $output1;
   if($result1 === Result::Ok) {
@@ -53,16 +53,16 @@ $parse_or_else = fn($parser1, $parser2) => function($input) use ($parser1, $pars
 $null_safe_callback = fn($callback) => fn($carry, $item) =>
   $carry === NULL ? $item : $callback($carry, $item);
 
-$parse_choice = fn($parsers) =>
-  array_reduce($parsers, $null_safe_callback($parse_or_else));
+$p_choice = fn($parsers) =>
+  array_reduce($parsers, $null_safe_callback($p_or_else));
 
 
-$parse_a = $parse_word('a');
-$parse_b = $parse_word('b');
-$parse_c = $parse_word('c');
-$parse_b_or_c = $parse_choice([$parse_b, $parse_c]);
-$parse_a_and_then_b_or_c = $parse_and_then($parse_a, $parse_b_or_c);
-$parse_a_or_b = $parse_or_else($parse_a, $parse_b);
+$parse_a = $p_string('a');
+$parse_b = $p_string('b');
+$parse_c = $p_string('c');
+$parse_b_or_c = $p_choice([$parse_b, $parse_c]);
+$parse_a_and_then_b_or_c = $p_and_then($parse_a, $parse_b_or_c);
+$parse_a_or_b = $p_or_else($parse_a, $parse_b);
 echo '<pre>';
 print_r($parse_a_and_then_b_or_c('abz'));
 print_r($parse_a_and_then_b_or_c('acz'));
