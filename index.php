d392dd9a3bf22e71d0314c0af1baafc8dd2ad7d0
César González <chesarglez429@gmail.com>
2023-09-17 05:56:00 -0600

    :construction: Test database connection

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 include './ParseQuery.php';
+include './db_connection.php';
+
+
+$dummy_query = "SELECT id, product_code, product_name FROM products";
+$rows = mysqli_fetch_all(mysqli_query($db_conection, $dummy_query));
 
 
 $p_doublequote = $p_string('"');
@@ -11,6 +16,7 @@ $p_optional_semicolon = $p_optional($p_string(';'));
 $p_digit_then_semicolon = $p_and_then($p_digit, $p_optional_semicolon);
 $p_digits_separated_by_comma = $p_trim($p_separated_by_1($p_digit, $p_comma));
 echo '<pre>';
+var_dump($rows);
 var_dump($p_query('Potato Chips'));
 var_dump($p_query('Potato AND Chips'));
 var_dump($p_query('Potato AND NOT Chips'));
@@ -20,4 +26,7 @@ var_dump($p_query('CAMPOS(suppliers.company)'));
 var_dump($p_query('CAMPOS(suppliers.company, suppliers.job_title)'));
 var_dump($p_query('Papas Potato AND NOT Chips AND CADENA (con chile) OR PATRON (sabri) CAMPOS (products.description)'));
 echo '</pre>';
+
+mysqli_close($db_conection);
+
 ?>
