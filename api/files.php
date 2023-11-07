350b49500f77bef3f8c9c6d97d4edc3582c8a8d1
César González <chesarglez429@gmail.com>
2023-11-07 05:07:47 -0600

    :construction: Get the tokens of the files

--- api/files.php
+++ api/files.php
@@ -1,5 +1,6 @@
 <?php
 include './pdo.php';
+include './ParserLibrary.php';
 
 // Copied from https://stackoverflow.com/a/67111853/13194448
 function array_every(array $arr, callable $predicate) {
@@ -20,6 +21,42 @@ if (!array_every($filenames, $is_txt)) {
   echo "Not all files are txt";
   exit();
 }
+
+$file_contents =
+  array_map("file_get_contents", $_FILES["files"]["tmp_name"]);
+
+$sanitize = fn($content) =>
+  strtolower(
+    iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE',
+      preg_replace('/\p{P}/', ' ',
+        $content
+      )
+    )
+  );
+
+$sanitized_file_contents =
+  array_map($sanitize, $file_contents);
+
+
+$p_token =
+  $p_concatenate(
+    $p_one_or_more(
+      $p_satisfy_char('ctype_alnum')
+    )
+  );
+
+$p_tokens = 
+  $p_trim(
+    $p_separated_by(
+      $p_token,
+      $p_one_or_more_whitespaces
+    )
+  );
+
+
+$tokens_in_files = 
+  array_map($p_tokens, $sanitized_file_contents);
+
 
 $documentos = [];
 
