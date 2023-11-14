783d8e22c8f957eb57880e4567ac61ba2579e1ca
César González <chesarglez429@gmail.com>
2023-11-14 17:58:44 -0600

    :construction: Perform the search in the vocabulary instead of some user-provided table

--- api/ParseQuery.php
+++ api/ParseQuery.php
@@ -99,12 +99,9 @@ $p_operator =
 
 $p_tokens =
   $p_separated_by(
-    $p_exception(
-      $p_opener('CAMPOS'),
-      $p_or_else(
-        $p_operator,
-        $p_terms
-      )
+    $p_or_else(
+      $p_operator,
+      $p_terms
     ),
     $p_one_or_more_whitespaces
   );
@@ -147,90 +144,10 @@ $insert_missing_or_operators = function($expression_tokens) use (&$insert_missin
 };
 
 
-$p_expressions =
+$p_query =
   $p_map(
     $insert_missing_or_operators,
     $p_tokens
-  );
-
-
-$p_table_name =
-  $p_concatenate(
-    $p_one_or_more(
-      $p_or_else(
-        $p_alphanumeric,
-        $p_string('_')
-      )
-    )
-  );
-
-$p_column_name =
-  $p_table_name;
-
-$p_field =
-  $p_around(
-    $p_table_name,
-    $p_string('.'),
-    $p_column_name
-  );
-
-$p_field_separator =
-  $p_and_then(
-    $p_string(','),
-    $p_zero_or_more_whitespaces
-  );
-
-$p_fields =
-  $p_between(
-    $p_opener('CAMPOS'),
-    $p_separated_by_1(
-      $p_field,
-      $p_field_separator
-    ),
-    $p_string(')')
-  );
-
-
-$fields_by_table = function($tables_so_far, $field) {
-  [$table_name, $field_name] = $field;
-
-  if (array_key_exists($table_name, $tables_so_far)) {
-    $new_table = $tables_so_far[$table_name];
-    array_push($new_table, $field_name);
-    $new_tables = $tables_so_far;
-    $new_tables[$table_name] = $new_table;
-    return $new_tables;
-  }
-
-  $new_tables = $tables_so_far;
-  $new_tables[$table_name] = [$field_name];
-  return $new_tables;
-};
-
-
-$default_tables = [
-  "products" => [
-    "product_name",
-    "quantity_per_unit",
-    "category"
-  ]
-];
-
-
-$p_tables = 
-  $p_map(
-    fn($fields) =>
-      match ($fields[0]) {
-        Maybe::Nothing => $default_tables,
-        Maybe::Some => array_reduce($fields[1], $fields_by_table, []),
-      },
-    $p_optional($p_fields)
-  );
-
-$p_query = 
-  $p_and_then(
-    $p_expressions,
-    $p_tables
   );
 
 
@@ -280,25 +197,9 @@ $token_to_sql_condition = function($column_names) use (&$token_to_sql_condition,
     };
 };
 
-$table_to_sql_query = fn($expression_tokens) => function($table_name, $column_names) use ($expression_tokens, $token_to_sql_condition) {
-  $where_statement =
-    count($expression_tokens) === 0
-    ? ''
-    : "WHERE " . implode(' ', array_map($token_to_sql_condition($column_names), $expression_tokens));
 
-  return [$table_name, "SELECT * FROM $table_name $where_statement;"];
-};
-
-function array_map_assoc(callable $f, array $a) {
-    return array_column(array_map($f, array_keys($a), $a), 1, 0);
-}
-
-$query_to_sql_query = function($query_result) use ($table_to_sql_query) {
-  [$expression_tokens, $tables] = $query_result;
-  return array_map_assoc(
-    $table_to_sql_query($expression_tokens),
-    $tables
-  );
+$query_to_sql_query = function($expression_tokens) use ($token_to_sql_condition) {
+  return "SELECT * FROM Vocabulario WHERE " . implode(' ', array_map($token_to_sql_condition(["token"]), $expression_tokens));
 };
 
 
@@ -328,7 +229,7 @@ $p_sql_query =
 
 $p_sql_results = 
   $p_map(
-    fn($sql_queries_by_table) => array_map($make_sql_request, array_keys($sql_queries_by_table), array_values($sql_queries_by_table)),
+    fn($sql_query) => $make_sql_request("Vocabulario", $sql_query),
     $p_sql_query
   );
 
